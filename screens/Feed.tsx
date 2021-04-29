f788a7156a33f609a339c2685ac8773ccd19b1e0
aureuneun <aureuneun@gmail.com>
2021-04-29 21:48:08 +0900

    feat: pull to refresh

--- screens/Feed.tsx
+++ screens/Feed.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
-import React from 'react';
+import React, { useState } from 'react';
 import { ListRenderItemInfo, FlatList, Text } from 'react-native';
 import Photo from '../components/Photo';
 import ScreenLayout from '../components/ScreenLayout';
@@ -29,16 +29,24 @@ const FEED_QUERY = gql`
 `;
 
 const Feed = ({ navigation }: Props) => {
-  const { data, loading } = useQuery<seeFeed>(FEED_QUERY);
+  const { data, loading, refetch } = useQuery<seeFeed>(FEED_QUERY);
+  const [refreshing, setRefreshing] = useState(false);
   const renderItem = ({
     item: photo,
   }: ListRenderItemInfo<seeFeed_seeFeed | null>) => {
     return photo ? <Photo {...photo} /> : null;
   };
+  const refresh = async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  };
   return (
     <ScreenLayout loading={loading}>
       {data?.seeFeed && (
         <FlatList
+          refreshing={refreshing}
+          onRefresh={refresh}
           style={{ width: '100%' }}
           showsVerticalScrollIndicator={false}
           data={data.seeFeed}
